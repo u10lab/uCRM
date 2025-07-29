337207be9414640f7cfab6c9cb4ed236e47dd3f2
u10lab <u10engineer@gmail.com>
2025-07-29 20:00:03 +0900

    specify version & npm install

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { route } from 'ziggy-js';
+</script>
+
+<template>
+<p>Inertiaテスト</p>
+<br>
+<a href="/">aタグ</a><br>
+<Link href="/">link</Link><br>
+<Link :href="route('inertia.index')">名前付きルートの確認</Link>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InertiaTestController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -15,6 +16,12 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+});
+
+Route::get('/inertia/index', [InertiaTestController::class, 'index'])
+   ->name('inertia.index');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
