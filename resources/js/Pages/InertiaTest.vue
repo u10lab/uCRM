83ef76e1b48a7036ab6aecb7d89384cfdd524d1d
u10lab <u10engineer@gmail.com>
2025-07-31 16:24:18 +0900

    feat: Implement demo for understanding Inertia.js and Laravel integration

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
 import { route } from 'ziggy-js';
+import { ref } from 'vue';
+
+const newTitle = ref()
+const newContent = ref()
 </script>
 
 <template>
@@ -8,5 +12,17 @@ import { route } from 'ziggy-js';
 <br>
 <a href="/">aタグ</a><br>
 <Link href="/">link</Link><br>
-<Link :href="route('inertia.index')">名前付きルートの確認</Link>
+<Link :href="route('inertia.index')">名前付きルートの確認</Link> <br>
+<Link :href="route('inertia.show',{id:1})">Linkルートの確認</Link>
+
+<div class="mb-8"></div>
+<input type="text" nmae="newTitle" v-model="newTitle">{{ newTitle }}<br>
+<input type="text" nmae="newContent" v-model="newContent">{{ newContent }}<br>
+<Link as="button" method="post" :href="route('inertia.store')"
+:data="{
+    title: newTitle,
+    content: newContent
+}">DB保存</Link>
+
+
 </template>
